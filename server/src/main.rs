e8b58f21520469de526f1de3f411138fd7735e12
Raspirin <f1193968991@126.com>
2024-06-01 18:09:20 +0800

    chore: cleanup

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,15 +2,27 @@ use rocket::{get, launch, routes, serde::json::Json, State};
 use server::{
     cors::CORS,
     parser::{TagContent, TagParser},
-    response::{Body, Item},
+    response::Body,
 };
 
 #[get("/search/<mode>/<keyword>")]
 fn search(mode: &str, keyword: &str, content: &State<TagContent>) -> Json<Body> {
     let items = match mode {
-        "name" => content.search_in_name(keyword).into_iter().map(|x| x.into()).collect(),
-        "para" => content.search_in_para(keyword).into_iter().map(|x| x.into()).collect(),
-        "rt" => content.search_in_rt(keyword).into_iter().map(|x| x.into()).collect(),
+        "name" => content
+            .search_in_name(keyword)
+            .into_iter()
+            .map(|x| x.into())
+            .collect(),
+        "para" => content
+            .search_in_para(keyword)
+            .into_iter()
+            .map(|x| x.into())
+            .collect(),
+        "rt" => content
+            .search_in_rt(keyword)
+            .into_iter()
+            .map(|x| x.into())
+            .collect(),
         _ => vec![],
     };
     let body = Body { items };
@@ -20,7 +32,7 @@ fn search(mode: &str, keyword: &str, content: &State<TagContent>) -> Json<Body> 
 #[launch]
 fn start() -> _ {
     let input = include_str!("cppreference-doxygen-web.tag.xml");
-    let content = TagParser.parse_doc(&input).unwrap();
+    let content = TagParser.parse_doc(input).unwrap();
 
     rocket::build()
         .manage(content)
