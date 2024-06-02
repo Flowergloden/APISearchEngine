9e687a474c79848948739c6e4af59a2f1b0b2b80
Raspirin <f1193968991@126.com>
2024-06-02 11:45:47 +0800

    fixL cors

--- server/src/cors.rs
+++ server/src/cors.rs
@@ -16,7 +16,7 @@ impl Fairing for CORS {
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
         response.set_header(Header::new(
             "Access-Control-Allow-Origin",
-            "http://localhost:5000",
+            "*",
         ));
         response.set_header(Header::new(
             "Access-Control-Allow-Methods",
